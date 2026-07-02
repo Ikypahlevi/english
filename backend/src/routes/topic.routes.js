3bdf8b90d1c7ac8a7743c8642bbee8832c6504a6
duong <duongss2208@gmail.com>
2026-07-02 18:19:57 +0700

    fix(backend): Khớp api route import excel với client

--- backend/src/routes/topic.routes.js
+++ backend/src/routes/topic.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const topicController = require('../controllers/topic.controller');
 const { authenticateToken, verifyAdmin } = require('../middlewares/auth.middleware');
 
-router.post('/upload-excel', authenticateToken, topicController.uploadExcel);
+router.post('/topics/import', authenticateToken, topicController.uploadExcel);
 router.get('/topics', authenticateToken, topicController.getTopics);
 router.get('/topics/:topicId/vocabularies', authenticateToken, topicController.getVocabularies);
 router.delete('/topics/:topicId', authenticateToken, topicController.deleteTopic);
